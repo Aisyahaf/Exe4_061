69610f399c496788c8013b57dfd5fbf92229addc
Aisah Fitriani <aisah.atik.ft21@mail.umy.ac.id>
2022-12-07 16:57:01 +0700

    Add Fixed Data Type

--- Exe4_061/Program.cs
+++ Exe4_061/Program.cs
@@ -8,18 +8,18 @@ namespace Exe4_061
 {
    class Stacks
    {
-        public int[] aisah;
+        public char [] aisah;
         public int top;
         public int max;
 
         public Stacks(int size)
         {
-            aisah = new int[size];
+            aisah = new char [size];
             top = -1;
             max = size;
         }
 
-        bool empty()
+        public bool empty()
         {
             if (top == -1)
                 return(true);
@@ -27,7 +27,7 @@ namespace Exe4_061
                 return(false);
         }
 
-        public void push(int item)
+        public void push(char item) //Method untuk menambahkan data/memasukkan data dalam stack
         {
             if( top == max - 1)
             {
@@ -35,11 +35,11 @@ namespace Exe4_061
             }
             else
             {
-                aisah[top++] = item;
+                aisah[++top] = item;
             }
         }
 
-        public int pop()
+        public int pop() //Method untuk mengeluarkan data / menghapus data dalam stack
         {
             if (top == -1)
             {
@@ -47,20 +47,21 @@ namespace Exe4_061
             }
             else
             {
-                Console.WriteLine("\nThe Poped element is :  " + aisah[top]);
+                Console.WriteLine("\nThe Poped Element Is :  " + aisah[top]);
                 return aisah[top--];
             }
         }
 
-        public void display()
+        public void display() //Method untuk menampilkan data
         {
             if (empty())
-                Console.WriteLine("\nStack Empty");
+                Console.WriteLine("\nStack Is Empty\n");
+
             else
             {
                 for (int i = 0; i <= top; i++)
                 {
-                    Console.WriteLine("Item [ " + (i + 1) + "] : " + aisah[i]);
+                    Console.WriteLine("Item [ " + (i + 1) + " ] : " + aisah [i] );
                 }
                 Console.WriteLine();
             }
@@ -77,21 +78,22 @@ namespace Exe4_061
                 Console.WriteLine("2. Pop");
                 Console.WriteLine("3. Display");
                 Console.WriteLine("4. Exit");
-                Console.Write("\nEnter your choice : ");
+                Console.Write("\nEnter Your Choice : ");
                 string sInput = Console.ReadLine();
                 char ch = Convert.ToChar(sInput == "" ? "0" : sInput);
                 switch (ch)
                 {
                     case '1':
-                        Console.Write("\nEnter a Number : ");
-                        int num = Convert.ToInt32(Console.ReadLine());
-                        s.push(num);
+                        Console.Write("\nEnter a character : ");
+                        string sInput1 = Console.ReadLine();
+                        char c = Convert.ToChar(sInput1 == "" ? "0" : sInput1);
+                        s.push(c);
                         break;
 
                     case '2':
                         if (s.empty())
                         {
-                            Console.WriteLine("\nStack Is Empty");
+                            Console.WriteLine("\n===========Stack Is Empty============\n");
                             break;
                         }
                         s.pop();
@@ -100,8 +102,10 @@ namespace Exe4_061
                     case '3':
                         s.display();
                         break;
+
                     case '4':
                         return;
+
                     default:
                         Console.WriteLine("\nInvalid Choice");
                         break;
@@ -111,3 +115,6 @@ namespace Exe4_061
 
    }
 }
+
+//kumpulan data yg diakses hanya d salah satu ujung nya, yaitu ujung atas
+//data yang trkhir di tambahkan akan pertama kali di kluarkan 
