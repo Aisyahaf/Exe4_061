02bf16b574c588c93058967307148f49e0b14c2a
Aisah Fitriani <aisah.atik.ft21@mail.umy.ac.id>
2022-12-07 15:39:49 +0700

    Add Class Stacks & Add Variabel

--- Exe4_061/Program.cs
+++ Exe4_061/Program.cs
@@ -6,10 +6,27 @@ using System.Threading.Tasks;
 
 namespace Exe4_061
 {
-    internal class Program
-    {
-        static void Main(string[] args)
+   class Stacks
+   {
+        public int[] element;
+        public int top;
+        public int max;
+
+        public Stacks(int size)
         {
+            element = new int[size];
+            top = -1;
+            max = size;
         }
+
+        bool empty()
+        {
+            if (top == -1)
+                return(true);
+            else
+                return(false);
+        }
+
+        
     }
 }
